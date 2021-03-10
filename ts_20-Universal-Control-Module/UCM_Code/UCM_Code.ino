78cb109716c1a4d53ff939bfdd7410b33f3ef1b5
Nam Tran <namtran211101@gmail.com>
2021-03-10 11:39:58 +1100

    UCM Code updated with all input running

--- ts_20-Universal-Control-Module/UCM_Code/UCM_Code.ino
+++ ts_20-Universal-Control-Module/UCM_Code/UCM_Code.ino
@@ -3,10 +3,10 @@
 #include <eXoCAN.h>
 
 //Declarations
-Adafruit_ADS1115 ads;
-#define SERIAL_UART_INSTANCE    1 //ex: 2 for Serial2 (USART2)
-#define PIN_SERIAL_RX           PA10
-#define PIN_SERIAL_TX           PA9
+Adafruit_ADS1115 ads(0x49);
+#define Serial1_UART_INSTANCE    1 //ex: 2 for Serial12 (USART2)
+#define PIN_Serial1_RX           PA10
+#define PIN_Serial1_TX           PA9
 #define TXLED PB0
 #define RXLED PB1
 
@@ -24,14 +24,19 @@ int16_t adc0, adc1, adc2, adc3;
 
 // tx frame setup #1
 int UCMID = 0x069;
+int UCM_Heartbeat = 0x068;
 int rxMsgID = 0x005;   // needed for rx filtering
 uint8_t txData[8];
 uint8_t txDataLen = 8;
-uint32_t txDly = 5000; //Message Transmit Frequency in Milliseconds (Transmits every 5 seconds atm)
+uint32_t txDly = 100; //Message Transmit Frequency in Milliseconds (Transmits every 5 seconds atm)
 uint32_t last = 0;
 int id, fltIdx;
 uint8_t rxbytes[8];
 uint8_t rxData[8];
+uint32_t last_counter = 0;
+uint32_t Heartbeat_Counter = 0;
+uint8_t Heartbeat_Data[2];
+uint8_t Heartbeat_State = 0;
 eXoCAN can;
 
 // the setup function runs once when you press reset or power the board
@@ -40,7 +45,6 @@ void setup() {
   // initialize digital pin PC_13 as an output.
   // for troubleshooting to ensure the board is comunicating andissuing comands
   pinMode(PC13, OUTPUT);
-
 //--------------------------------------------------//
 //Configuring the Digital Input Pins. Uncomment when required.
 //--------------------------------------------------//
@@ -72,19 +76,19 @@ void setup() {
   // //ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   // //ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
 
-  //ads.begin();
+  ads.begin();
   
 //--------------------------------------------------//
 // Initiallising CAN
 //--------------------------------------------------//
- can.begin(STD_ID_LEN, BR250K, PORTB_8_9_XCVR);           // 11b IDs, 250k bit rate, Pins 8 and 9 with a transceiver chip
+ can.begin(STD_ID_LEN, BR500K, PORTB_8_9_XCVR);           // 11b IDs, 250k bit rate, Pins 8 and 9 with a transceiver chip
  can.filterMask16Init(0, 0, 0x7ff, 0, 0);                 // filter bank 0, filter 0: don't pass any, flt 1: pass all msgs
   pinMode(PB0, OUTPUT);                                   // TX Indicator Pin
   pinMode(PB1, OUTPUT);                                   // RX Indicator Pin 
 //--------------------------------------------------//
-// Initiallising Serial
+// Initiallising Serial1
 //--------------------------------------------------//
-  Serial.begin(9600);
+  Serial1.begin(9600);
 }
 
 
@@ -93,34 +97,40 @@ void setup() {
 //--------------------------------------------------//
  void CANTransmit(){
 
-//--------------------------------------------------//
-// Digital Input Value Fill
-//--------------------------------------------------//
+// Digital Input Value Fill ------------------------//
   txData[0]=DigIN1;
   txData[1]=DigIN2;
   txData[2]=DigIN3;
   txData[3]=DigIN4;
 
-
-//--------------------------------------------------//
-// Analogue Input Value Fill
-//--------------------------------------------------//
+// Analogue Input Value Fill ----------------------//
   txData[4]=adc0;
   txData[5]=adc1;
   txData[6]=adc2;
   txData[7]=adc3;
 
-
-//--------------------------------------------------//
-// Transmit data
-//--------------------------------------------------//
-    if (millis() / txDly != last)             // tx every txDly
+// Transmit Hearbeat ----------------------------------//
+    if (millis() - last_counter >= 1000)             // tx every txDly
        {
           digitalToggle(TXLED);
-          last = millis() / txDly;
-          can.transmit(txMsgID, txData, txDataLen);
+          last_counter = millis();
+          Heartbeat_Data[0] = Heartbeat_State;
+          Heartbeat_Data[1] = Heartbeat_Counter;
+          can.transmit(UCM_Heartbeat, Heartbeat_Data, 2);
+          Heartbeat_Counter = Heartbeat_Counter + 1;
           digitalToggle(TXLED);
-          Serial.println("Transmitting...");
+          //Serial1.println("Transmitting heartbeat...");
+  };
+
+// Transmit data ----------------------------------//
+    if (millis() / txDly != last)             // tx every txDly
+       {
+          
+          digitalToggle(TXLED);
+          last = millis() / txDly;
+          can.transmit(UCMID, txData, txDataLen);
+          digitalToggle(TXLED);
+          //Serial1.println("Transmitting...");
   }
     };
 
@@ -131,8 +141,8 @@ void setup() {
     if (can.receive(id, fltIdx, rxbytes) > -1) // poll for rx
       {
        digitalToggle(RXLED);
-       rxData[]=rxbytes;
-       Serial.println("Receiving...");
+       //rxData[]=rxbytes;
+       Serial1.println("Receiving...");
        digitalToggle(RXLED);
       }
    };
@@ -148,24 +158,25 @@ void digitalupdate(){
 }
 
 //--------------------------------------------------//
-// This Function Prints all values to Serial
+// This Function Prints all values to Serial1
 //--------------------------------------------------//
-void SERIALPRINT() {
+void Serial1PRINT() {
 
   //Digital Input 
-  Serial.print("Digital Input 1: ");  Serial.println(DigIN1); 
-  Serial.print("Digital Input 2: ");  Serial.println(DigIN2); 
-  Serial.print("Digital Input 3: ");  Serial.println(DigIN3); 
-  Serial.print("Digital Input 4: ");  Serial.println(DigIN4); 
+  Serial1.print("Digital Input 1: ");  Serial1.println(DigIN1); 
+  Serial1.print("Digital Input 2: ");  Serial1.println(DigIN2); 
+  Serial1.print("Digital Input 3: ");  Serial1.println(DigIN3); 
+  Serial1.print("Digital Input 4: ");  Serial1.println(DigIN4); 
   //Analogue Input
-  Serial.print("Analogue Input 0: "); Serial.println(adc0);
-  Serial.print("Analogue Input 1: "); Serial.println(adc1);
-  Serial.print("Analogue Input 2: "); Serial.println(adc2);
-  Serial.print("Analogue Input 3: "); Serial.println(adc3);
-  Serial.println(" ");
+  Serial1.print("Analogue Input 0: "); Serial1.println(adc0);
+  Serial1.print("Analogue Input 1: "); Serial1.println(adc1);
+  Serial1.print("Analogue Input 2: "); Serial1.println(adc2);
+  Serial1.print("Analogue Input 3: "); Serial1.println(adc3);
+  Serial1.println(" ");
   //Driver Value
-  Serial.print("Driver 1: "); Serial.println(DriveValue1);
-  Serial.print("Driver 2: "); Serial.println(DriveValue2);
+  Serial1.print("Driver 1: "); Serial1.println(DriveValue1);
+  Serial1.print("Driver 2: "); Serial1.println(DriveValue2);
+  delay(300);
   }
 
 //--------------------------------------------------//
@@ -192,32 +203,49 @@ digitalWrite(PB14, HIGH);
 //--------------------------------------------------//
 // Conditional Output Driver.
 //--------------------------------------------------//
-  if(DriveCondition1 == DriveRequirment1){
-  digitalWrite(PB15, HIGH);
-  DriveValue1 = 1;
-  }
-  else {
-  digitalWrite(PB15, LOW);
-  DriveValue1 = 0;
-  }
+//  if(DriveCondition1 == DriveRequirment1){
+//  digitalWrite(PB15, HIGH);
+//  DriveValue1 = 1;
+//  }
+//  else {
+//  digitalWrite(PB15, LOW);
+//  DriveValue1 = 0;
+//  }
+//
+//    if(DriveCondition2 == DriveRequirment2){
+//  digitalWrite(PB14, HIGH);
+//  DriveValue2 = 1;
+//  }
+//  else {
+//  digitalWrite(PB14, LOW);
+//  DriveValue2 = 0;
+//  }
 
-    if(DriveCondition2 == DriveRequirment2){
-  digitalWrite(PB14, HIGH);
-  DriveValue2 = 1;
-  }
-  else {
-  digitalWrite(PB14, LOW);
-  DriveValue2 = 0;
-  }
+//--------------------------------------------------//
+// Conditional Output Driver w/ PWM.
+//--------------------------------------------------//
+//    if(DriveCondition1 == DriveRequirment1){
+//  digitalToggle(PB15);
+//  DriveValue1 = 1;
+//
+//
+//    if(DriveCondition2 == DriveRequirment2){
+//  digitalWrite(PB14, HIGH);
+//  DriveValue2 = 1;
+//  }
+//  else {
+//  digitalWrite(PB14, LOW);
+//  DriveValue2 = 0;
+//  }
+//}
 }
-
 // Arduinos "Main" function //
 void loop() {
   digitalupdate();
   I2C();
   Driver();
+  //Serial1PRINT();
   CANTransmit();
   CANRecieve();
-  digitalToggle(PC13);
-  delay(100);   
+  digitalToggle(PC13); 
 }
